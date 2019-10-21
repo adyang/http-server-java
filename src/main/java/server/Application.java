eccd3d2da63fffd06fbe4a1777925637382b6b13
adyang <20383270+adyang@users.noreply.github.com>
2019-10-22 03:34:28 +0800

    Init repository with Gradle Application plugin.

---
+++ src/main/java/server/Application.java
@@ -0,0 +1,10 @@
+package server;
+
+import java.util.Arrays;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("args: " + Arrays.toString(args));
+    }
+}
